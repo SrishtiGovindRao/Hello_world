2c8bb5c78e7330de9f6b09959cd159405a212079
“Abhishek <abhisheks.mobcoder@gmail.com>
2022-04-28 20:48:40 +0530

    dshdkjs

--- hello.php
+++ hello.php
@@ -5,14 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
-<body>
-    <h1>Hello World</h1>
-    <?php
-        echo "Hello World";
-    ?>
-    <?php echo "hello world"?>
-    <h1>Hello World2</h1>
     
 </body>
 </html>
